4febd6831f32acf822a67a48d10967ba00289854
JITHENTHIRIYA <jithenthiriya.19cs@kct.ac.in>
2021-06-23 12:01:03 -0700

    Addition of SearchBox

--- src/components/App/APP.jsx
+++ src/components/App/APP.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Header from '../Header/Header';
+import SearchBox from '../SearchBox/SearchBox';
 import './App.css';
 
 class App extends React.Component {
 
+    state = {
+        headerText: "Name It",
+    };
+
     render() {
         return (
             <div>
-                <Header />
+                <Header headTitle={this.state.headerText} />
+                <SearchBox />
             </div>
         );
     }
 }
 
 
-export default App
+export default App;
 
 
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchBox.css';
+
+const SearchBox = () => {
+    return (
+
+        <div className="search-container">
+            <input placeholder="Type Keywords" className="search-input" />
+
+        </div>
+    );
+};
+
+export default SearchBox;
