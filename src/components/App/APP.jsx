11a66c76e5a894e12b7202018889a5d60d837a58
JITHENTHIRIYA <69334023+JITHENTHIRIYA@users.noreply.github.com>
2021-06-25 17:52:48 -0700

    Implemented react Hooks by removing the class based component

--- src/components/App/APP.jsx
+++ src/components/App/APP.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import SearchBox from '../SearchBox/SearchBox';
@@ -6,33 +6,29 @@ import './App.css';
 
 const name = require('@rstacruz/startup-name-generator');
 
-class App extends React.Component {
+const App = () => {
 
-    state = {
-        headerText: "Name It",
-        headerExpanded: true,
-        suggestedNames: [],
-    };
+    const [headerText, setHeaderText] = useState('Name It!');
+    const [headerExpanded, setHeaderExpanded] = useState(true);
+    const [suggestedNames, setSuggestedNames] = useState([]);
 
-    handleInputChange = (inputText) => {
+    const handleInputChange = (inputText) => {
+        setHeaderExpanded(!inputText);
+        setSuggestedNames(inputText ? name(inputText) : []);
 
-        this.setState({
-            headerExpanded: !inputText,
-            suggestedNames: name(inputText),
-        });
     }
 
-    render() {
 
-        return (
-            <div>
-                <Header headerExpanded={this.state.headerExpanded} headTitle={this.state.headerText} />
-                <SearchBox handleInputChange={this.handleInputChange} />
-                <ResultsContainer suggestedNames={this.state.suggestedNames} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Header headerExpanded={headerExpanded} headTitle={headerText} />
+            <SearchBox handleInputChange={handleInputChange} />
+            <ResultsContainer suggestedNames={suggestedNames} />
+        </div>
+    );
 }
+
+
 
 
 export default App;
